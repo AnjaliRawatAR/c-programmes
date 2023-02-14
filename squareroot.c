704a72fc293713f21004d0698db95709f4f320a9
AnjaliRawatAR <114013299+AnjaliRawatAR@users.noreply.github.com>
2023-02-15 00:27:20 +0530

    Create squareroot.c

---
+++ squareroot.c
@@ -0,0 +1,12 @@
+//to find the square root of any number
+
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+    int x;
+    printf("Enter the number:\n");
+    scanf ("%d", &x);
+    printf ("the number is :%d",x);
+    printf ("\nthus the Square root is: %f",sqrt(x));
+}
